f88835e29fd27e3aac69b2f838ee181850a6ae7d
emerson <emerson91030@gmail.com>
2016-07-23 17:06:10 -0700

    Use find and insert in collection

--- client/main.js
+++ client/main.js
@@ -3,9 +3,10 @@ import angularMeteor from 'angular-meteor';
 import templateHome from './template/home.html';
 import templateForecast from './template/forecast.html';
 import templateSample from './directives/sample.html';
+import { Pb } from '/lib/collections.js'
 
 // module
-var tym = angular.module("tym", [angularMeteor, require('angular-route')]);
+const tym = angular.module("tym", [angularMeteor, require('angular-route')]);
 
 // routes
 tym.config(function($routeProvider) {
@@ -29,12 +30,31 @@ tym.directive("sample", function() {
 });
 
 // controllers
-tym.controller("homeCtrl", ["$scope", function($scope) {
+tym.controller("homeCtrl", ["$scope", "$reactive", function($scope, $reactive) {
+	$reactive(this).attach($scope);
+
 	$scope.page = "home";
 	console.log($scope.page);
+
+	this.helpers({
+	    myData: () => {
+	    	// find pb item
+	    	console.log(Pb.find({_id:"NaJPNTs7aZZ5PHcFp"}).fetch());
+	      //return Pb.find({});
+	    },
+	    addCin: () => {
+	    	// insert in pb
+	    	Pb.insert({
+	    		name: "cin2",
+	    	});
+	    },
+  	});
 }]);
 
 tym.controller("forecastCtrl", ["$scope", function($scope) {
 	$scope.page = "forecast";
 	console.log($scope.page);
 }]);
+
+//console.log(Pb.find({name : "em"}));
+
